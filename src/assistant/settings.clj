dd6bfac6b1ce735cb3fd58e60b8689cebcd26123
Kyle Erhabor <kyleerhabor@gmail.com>
2021-11-21 23:10:25 -0500

    Remove blank config file

--- src/assistant/settings.clj
+++ src/assistant/settings.clj
@@ -1,6 +1,5 @@
 (ns assistant.settings
   (:require [clojure.edn :as edn]))
 
-(def config (edn/read-string (slurp "config.edn")))
 (def deps (edn/read-string (slurp "deps.edn")))
 (def secrets (edn/read-string (slurp "secrets.edn")))
