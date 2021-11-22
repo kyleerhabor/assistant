629e029146d2fb9c7bba72cff4c90a59a10bf481
Klay <kyleerhabor@gmail.com>
2021-11-22 10:05:25 -0500

    Remove unused port and mitigate potential null error

--- src/assistant/events.clj
+++ src/assistant/events.clj
@@ -1,7 +1,6 @@
 (ns assistant.events
   (:require [clojure.tools.logging :as log]
             [assistant.commands :refer [commands]]
-            [discljord.events.middleware]
             [discljord.formatting :refer [user-tag]]))
 
 (defmulti handler
@@ -12,7 +11,7 @@
   [conn _ interaction]
   (if (#{2 3} (:type interaction))
     ;; Let's just assume the command exists. Nothing should go wrong, right?
-    ((:command (commands (keyword (:name (:data interaction))))) conn interaction)))
+    ((:command ((keyword (:name (:data interaction))) commands)) conn interaction)))
 
 (defmethod handler :ready
   [_ _ data]
