6c6057e132d8265bd1521b299c6e7952e3af39fd
Kyle Erhabor <kyleerhabor@gmail.com>
2021-11-26 18:13:28 -0500

    Add tools.build

--- src/assistant/core.clj
+++ src/assistant/core.clj
@@ -1,4 +1,5 @@
 (ns assistant.core
+  (:gen-class)
   (:require [clojure.core.async :as async :refer [chan go]]
             [clojure.tools.logging :as log]
             [assistant.commands :refer [discord-commands]]
@@ -8,16 +9,21 @@
             [discljord.messaging :refer [bulk-overwrite-global-application-commands!
                                          get-current-application-information! start-connection! stop-connection!]]))
 
+(set! *warn-on-reflection* true)
+
 (defn -main [& [token]]
-  (let [event-ch (chan)
-        conn-ch (connect-bot! token event-ch
-                              :intents #{})
-        msg-ch (start-connection! token)]
-    (log/info :info "Application commands:"
-              @(bulk-overwrite-global-application-commands! msg-ch
-                                                            (:id @(get-current-application-information! msg-ch))
-                                                            discord-commands))
-    (message-pump! event-ch #(go (handler msg-ch %1 %2)))
-    (stop-connection! msg-ch)
-    (disconnect-bot! conn-ch)
-    (async/close! event-ch)))
+  (if token
+    (let [event-ch (chan)
+          conn-ch (connect-bot! token event-ch
+                                :intents #{})
+          msg-ch (start-connection! token)]
+      (log/info "Application commands:"
+                @(bulk-overwrite-global-application-commands! msg-ch
+                                                              (:id @(get-current-application-information! msg-ch))
+                                                              discord-commands))
+      (message-pump! event-ch #(go (handler msg-ch %1 %2)))
+      (stop-connection! msg-ch)
+      (disconnect-bot! conn-ch)
+      (async/close! event-ch))
+    (log/error "No token provided"))
+  (shutdown-agents))
